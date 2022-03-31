2c510b8c8aea75c33f9a36f6fd82d7d10057388a
Дмитрий Леганьков <dmitrijlegankov@MacBook-Air-Dmitrij.local>
2022-03-31 13:46:20 +0300

    feat: add filter

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  HostListener,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -11,17 +17,25 @@ export class HeaderComponent implements OnInit {
   public innerWidth: any;
   public activeBurger = false;
   public searchFieldIsActive = false;
-  @HostListener('window:resize', ['$event'])
+  public searchInputFocus = false;
+  public showInfoWrapper = true;
+
+  @ViewChild('search')
+  search!: ElementRef;
+  @ViewChild('headerWrapper')
+  headerWrapper!: ElementRef;
+
   ngOnInit(): void {
     this.getWidthFromWindow();
   }
+
+  @HostListener('window:resize', ['$event'])
   onResize() {
     this.getWidthFromWindow();
   }
 
   private getWidthFromWindow() {
     this.innerWidth = window.innerWidth;
-
     if (this.innerWidth <= 950) {
       this.isMobile = true;
     } else {
@@ -31,7 +45,20 @@ export class HeaderComponent implements OnInit {
   public burgerMenuToggle() {
     this.activeBurger = !this.activeBurger;
   }
-  public searchFieldToggle() {
-    this.searchFieldIsActive = !this.searchFieldIsActive;
+  public searchFieldToggle(event: Event) {
+    if (this.search.nativeElement == event.target) {
+      this.searchFieldIsActive = true;
+      if (this.isMobile && this.searchFieldIsActive) {
+        this.showInfoWrapper = false;
+      }
+    }
+    if (this.headerWrapper.nativeElement == event.target) {
+      this.searchFieldIsActive = false;
+      this.searchInputFocus = false;
+      this.showInfoWrapper = true;
+    }
+  }
+  public inputFocus() {
+    this.searchInputFocus = true;
   }
 }
